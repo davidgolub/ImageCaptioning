a896f88b82687ec27ff7399adfa889d8db31281e
David <golubd@cs.washington.edu>
2015-05-19 12:06:03 -0700

    Time checks added for forward/backward

--- models/ImageCaptioner.lua
+++ models/ImageCaptioner.lua
@@ -169,7 +169,7 @@ function ImageCaptioner:train(dataset)
         --  start5 - start4, start4 - start3, start3 - start2, start2 - start1)
       end
       local start8 = sys.clock()
-      print("Times are ", (start8 - start) / batch_size, tot_forward_diff / batch_size
+      print("Times are ", (start8 - start) / batch_size, tot_forward_diff / batch_size,
         tot_backward_diff / batch_size)
       tot_loss = tot_loss + loss
       loss = loss / batch_size
