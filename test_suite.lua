010a7629b553969a51c9c559748e31b372c137af
David <golubd@cs.washington.edu>
2015-05-24 16:13:15 -0700

    Added CPU checks to test suite

--- test_suite.lua
+++ test_suite.lua
@@ -10,9 +10,9 @@ include('tests/CpuChecks.lua')
 cpu_checker = imagelstm.CpuChecks{}
 cpu_checker:check_cpu()
 
--- gpu checks
--- gpu_checker = imagelstm.GpuChecks{}
--- gpu_checker:check_gpu()
+gpu checks
+gpu_checker = imagelstm.GpuChecks{}
+gpu_checker:check_gpu()
 
 -- grad checks
 grad_checker = imagelstm.GradChecks{}
