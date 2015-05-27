08bab2444d5bc15e60109d57d86c1d7f2b5250e6
David <golubd@cs.washington.edu>
2015-05-26 23:05:16 -0700

    Added training all layers

--- train_addlayer.sh
+++ train_addlayer.sh
@@ -1,12 +1,12 @@
 export PATH=/Users/david/torch/install/bin:$PATH
 
 th image_captioning/main.lua \
--optim adagrad \
+-optim rmsprop \
 -batch_size 33 \
 -mem_dim 150 \
 -epochs 300 \
 -combine_module addlayer \
--learning_rate 1.5 \
--emb_learning_rate 1.5 \
+-learning_rate 0.1 \
+-emb_learning_rate 0.001 \
 -gpu_mode
 | tee -a "log_addlayer_gpu.txt"
