607dfe98d6a2e5382e07a713a4e4271a839218ca
David <golubd@cs.washington.edu>
2015-06-02 06:46:07 -0700

    Added gpu training in add_layer.sh

--- train_addlayer.sh
+++ train_addlayer.sh
@@ -1,14 +1,13 @@
 export PATH=/Users/david/torch/install/bin:$PATH
 
 th image_captioning/main.lua \
--load_model \
--model_epoch 20 \
--optim rmsprop \
 -batch_size 33 \
 -mem_dim 150 \
+-emb_dim 50 \
 -epochs 300 \
--combine_module addlayer \
+-num_layers 2 \
+-gpu_mode \
+-combine_module singleaddlayer \
 -learning_rate 0.1 \
--emb_learning_rate 0.001 
-# -gpu_mode
-| tee -a "log_addlayer_gpu.txt"
+-optim rmsprop \
+| tee -a "log_singeaddlayer.txt"
