0d6a2e2c890ef5d0b1da302bf512631286979f8b
David <golubd@cs.washington.edu>
2015-06-20 08:37:35 -0700

    Kept input/output sentences as IntTensors instead of CudaTensors

--- models/ImageCaptioner.lua
+++ models/ImageCaptioner.lua
@@ -306,13 +306,6 @@ function ImageCaptioner:eval(dataset)
         local sentence = dataset.sentences[idx]
         local out_sentence = dataset.pred_sentences[idx]
 
-
-        if self.gpu_mode then
-          sentence = sentence:cuda()
-          out_sentence = out_sentence:cuda()
-          image_feats = image_feats:cuda()
-        end
-
         -- get text/image inputs
         local inputs = self.combine_layer:forward(sentence, image_feats)
         local hidden_inputs = self.hidden_layer:forward(image_feats)
