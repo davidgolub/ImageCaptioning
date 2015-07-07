35be9149b7857ac36dee7dc38b5d9526f583240a
David <golubd@cs.washington.edu>
2015-07-06 20:38:23 -0700

    Evaluating on coco

--- models.lua
+++ models.lua
@@ -13,7 +13,8 @@ params.image_dim = 1024
 params.emb_dim = 600
 params.mem_dim = 600
 params.learning_rate = 0.01
-params.data_dir = 'data/flickr8k/'
+params.data_dir = 'data'
+params.dataset = 'coco'
 params.emb_dir = 'data/glove/'
 params.combine_module = 'embedlayer'
 params.hidden_module = 'hiddenlayer'
@@ -77,40 +78,15 @@ function accuracy(pred, gold)
   return torch.eq(pred, gold):sum() / pred:size(1)
 end
 
-local opt_method
-if params.optim == 'adagrad' then
-  opt_method = optim.adagrad
+if params.dataset == 'coco' then
+  print("Loading coco dataset")
+  vocab, train_dataset, val_dataset, test_dataset =
+    imagelstm.read_coco_dataset(params.data_dir .. "/coco/", params.gpu_mode)
 else
-  opt_method = optim.rmsprop
+  vocab, train_dataset, val_dataset, test_dataset = 
+    imagelstm.read_flickr8k_dataset(params.data_dir .. "/flickr8k/", params.gpu_mode)
 end
 
-header('Image-Captioning with LSTMs')
-
--- directory containing dataset files
-local data_dir = params.data_dir
-
--- load vocab
-vocab = imagelstm.Vocab(data_dir .. 'vocab.txt', true)
-
--- load datasets
-
--- load train dataset
-local train_dir = data_dir
-train_dataset = imagelstm.read_caption_dataset(train_dir, vocab, params.gpu_mode,
-  'train')
-
--- load val dataset
-local val_dir = data_dir
-val_dataset = imagelstm.read_caption_dataset(val_dir, vocab, params.gpu_mode, 'val')
-
--- load test dataset
-local test_dir = data_dir
-test_dataset = imagelstm.read_caption_dataset(test_dir, vocab, params.gpu_mode, 
-  'test')
-
-
-collectgarbage()
-printf('num train = %d\n', train_dataset.size)
 
 model = imagelstm.ImageCaptioner.load("model_25.th")
 model:print_config()
@@ -118,4 +94,4 @@ model:print_config()
 --train_predictions = model:get_sentences(model:predict_dataset(train_dataset, 5, 30))
 --test_delpredictions = model:get_sentences(model:predict_dataset(test_dataset, 5, 30))
 --val_predictions = model:get_sentences(model:predict_dataset(test_dataset, 5, 30))
-evaluate_results(model, 1, 'flickr8k')
+evaluate_results(model, 1, 'coco')
